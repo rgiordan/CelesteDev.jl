36580eef23de2bc681f188115c82a485ca95725c
Ryan Giordano <rgiordan@gmail.com>
2016-08-09 16:00:05 -0700

    KL deriv tests

--- forward_diff/test_derivatives.jl
+++ forward_diff/test_derivatives.jl
@@ -23,52 +23,63 @@ import ElboDeriv: BvnComponent, GalaxyCacheComponent
 import ElboDeriv: eval_bvn_pdf!, get_bvn_derivs!, transform_bvn_derivs!
 
 cd("/home/rgiordan/Documents/git_repos/CelesteDev.jl/forward_diff")
-include("test_derivatives_functions.jl")
 include(joinpath(Pkg.dir("Celeste"), "test/derivative_utils.jl"))
 
 
-run, camcol, field = (3900, 6, 269)
+blob, ea, body = gen_two_body_dataset();
+keep_pixels = 10:11
+trim_tiles!(ea.images, keep_pixels);
 
-images = SDSSIO.load_field_images(run, camcol, field, datadir)
-tiled_images = TiledImage[TiledImage(img) for img in images]
-fname = @sprintf "%s/photoObj-%06d-%d-%04d.fits" datadir run camcol field
-catalog = SDSSIO.read_photoobj_celeste(fname)
+ea.active_sources = [1]
+vp_vec = wrap_vp_vector(ea, true);
+@time elbo_1 = ElboDeriv.elbo(ea);
+@time elbo_1 = ElboDeriv.elbo(ea);
 
-# Pick an object.
-objid = "1237662226208063499"
-objids = [ce.objid for ce in catalog]
-sa = findfirst(objids, objid)
-neighbors = Infer.find_neighbors([sa], catalog, tiled_images)[1]
 
-cat_local = vcat(catalog[sa], catalog[neighbors])
-vp = Vector{Float64}[init_source(ce) for ce in cat_local]
-patches, tile_source_map = Infer.get_tile_source_map(tiled_images, cat_local)
-ea = ElboArgs(tiled_images, vp, tile_source_map, patches, [1])
-Infer.fit_object_psfs!(ea, ea.active_sources)
-Infer.trim_source_tiles!(ea)
 
-elbo = ElboDeriv.elbo(ea);
 
-function wrap_elbo{NumType <: Number}(vs1::Vector{NumType})
-    ea_local = forward_diff_model_params(NumType, ea)
-    ea_local.vp[1][:] = vs1
-    local_elbo = ElboDeriv.elbo(ea_local, calculate_derivs=false)
-    local_elbo.v[1]
+#
+# # vp_vec is a vector of the parameters from all the active sources.
+function wrap_elbo{NumType <: Number}(vp_vec::Vector{NumType})
+    ea_local = unwrap_vp_vector(vp_vec, ea)
+    elbo = ElboDeriv.elbo(ea_local, calculate_derivs=false)
+    elbo.v[1]
 end
 
-@time ad_grad = ForwardDiff.gradient(wrap_elbo, ea.vp[1]);
-@time ad_hess = ForwardDiff.hessian(wrap_elbo, ea.vp[1]);
-@time ad_hess = ForwardDiff.hessian(wrap_elbo, ea.vp[1]);
+@time ad_grad = ForwardDiff.gradient(wrap_elbo, vp_vec)
+@time ad_hess = ForwardDiff.hessian(wrap_elbo, vp_vec)
 
-# julia> @time ad_hess = ForwardDiff.hessian(wrap_elbo, ea.vp[1]);
-# 381.400357 seconds (331.23 M allocations: 6.106 GB, 0.88% gc time)
-# julia> @time ad_hess = ForwardDiff.hessian(wrap_elbo, ea.vp[1]);
-# 6.854772 seconds (1.70 M allocations: 895.854 MB, 9.58% gc time
+@time test_with_autodiff(wrap_elbo, vp_vec, elbo_1);
 
-# Sanity check
-ad_v = wrap_elbo(ea.vp[1]);
-@test_approx_eq ad_v elbo.v
 
-hcat(ad_grad, elbo.d[:, 1])
-@test_approx_eq ad_grad elbo.d[:, 1]
-@test_approx_eq ad_hess elbo.h
+
+
+# include(joinpath(Pkg.dir("Celeste"), "src/elbo_kl.jl"))
+
+s = 1
+test_symb = :kl
+function wrap_kl{NumType <: Number}(vp_vec::Vector{NumType})
+    ea_local = unwrap_vp_vector(vp_vec, ea);
+    kl_source = zero_sensitive_float(CanonicalParams, NumType, length(ea.active_sources))
+    if test_symb == :kl
+        ElboDeriv.subtract_kl!(ea_local, kl_source, calculate_derivs=false)
+    elseif test_symb == :a
+        ElboDeriv.subtract_kl_a!(ea_local.vp[s], kl_source, false)
+    elseif test_symb == :r
+        ElboDeriv.subtract_kl_r!(ea_local.vp[s], kl_source, false)
+    elseif test_symb == :k
+        ElboDeriv.subtract_kl_k!(ea_local.vp[s], kl_source, false)
+    elseif test_symb == :c
+        ElboDeriv.subtract_kl_c!(ea_local.vp[s], kl_source, false)
+    else
+        println("Bad symbol")
+    end
+
+    kl_source.v[1]
+end
+
+accum = zero_sensitive_float(CanonicalParams, Float64, length(ea.active_sources))
+# ElboDeriv.subtract_kl_c!(ea.vp[s], accum, true);
+ElboDeriv.subtract_kl!(ea, accum, calculate_derivs=true);
+
+@time test_with_autodiff(wrap_kl, vp_vec, accum);
