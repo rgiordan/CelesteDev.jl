dfaf9fb05b73ee827168adf7a0f479f901e6f80c
Ryan Giordano <rgiordan@gmail.com>
2016-11-30 13:52:56 -0800

    Optimization working

--- rasterized_psf/galsim_check.jl
+++ rasterized_psf/galsim_check.jl
@@ -22,50 +22,84 @@ band_pixels = [
     extensions[index].pixels for index in first_band_index:(first_band_index + 4)
 ];
 assert_counts_match_expected_flux(band_pixels, header, iota);
-band_images = make_images(band_pixels, psf, wcs, header["CL_SKY"], iota);
+images = make_images(band_pixels, psf, wcs, header["CL_SKY"], iota);
 catalog_entries = make_catalog_entries(header);
 
 target_source = 1
 target_entry = catalog_entries[target_source]
 
-neighbor_map = Infer.find_neighbors([ target_source ], catalog_entries, band_images);
+neighbor_map = Infer.find_neighbors([ target_source ], catalog_entries, images);
 cat_local = vcat(target_entry, catalog_entries[neighbor_map[target_source]]);
 vp = Vector{Float64}[init_source(ce) for ce in cat_local];
-patches = Infer.get_sky_patches(band_images, cat_local);
-Infer.load_active_pixels!(band_images, patches);
+patches = Infer.get_sky_patches(images, cat_local);
+Infer.load_active_pixels!(images, patches);
 
-ea = ElboArgs(band_images, vp, patches, [1]);
-f_evals, max_f, max_x, nm_result = DeterministicVI.maximize_f(DeterministicVI.elbo, ea);
-
-
-
-
+ea = ElboArgs(images, vp, patches, [1]);
+f_evals, max_f, max_x, nm_result =
+    DeterministicVI.maximize_f(DeterministicVI.elbo, ea, verbose=false);
+vp_opt = deepcopy(ea.vp[1]);
 
 
+################
+# FFT version
 
+# using StaticArrays
+# import Celeste.Model
+# import Celeste.Model.SkyPatch
+using Celeste.PSF
+using Celeste.DeterministicVIImagePSF
+using PyPlot
 
+ea_fft = ElboArgs(images, deepcopy(vp), patches, [1], psf_K=1);
+Celeste.Infer.load_active_pixels!(images, patches; exclude_nan=false);
 
+psf_image_mat = Matrix{Float64}[
+    PSF.get_psf_at_point(ea.patches[s, b].psf) for s in 1:ea.S, b in 1:ea.N];
+fsm_vec = DeterministicVIImagePSF.FSMSensitiveFloatMatrices[
+    DeterministicVIImagePSF.FSMSensitiveFloatMatrices() for b in 1:ea_fft.N];
+DeterministicVIImagePSF.initialize_fsm_sf_matrices!(fsm_vec, ea_fft, psf_image_mat);
 
+function elbo_fft_opt{NumType <: Number}(
+                    ea::ElboArgs{NumType};
+                    calculate_derivs=true,
+                    calculate_hessian=true)
+    @assert ea.psf_K == 1
+    DeterministicVIImagePSF.elbo_likelihood_with_fft!(ea, 2, fsm_vec);
+    DeterministicVI.subtract_kl!(ea, ea.elbo_vars.elbo)
+    return deepcopy(ea.elbo_vars.elbo)
+end
 
-single_infer_variational_parameters = parallel_inference(band_images, catalog_entries,
-                                                         joint_infer=false);
-joint_infer_variational_parameters = parallel_inference(band_images, catalog_entries,
-                                                        joint_infer=true);
+# For compilation
+#include(joinpath(dir, "rasterized_psf/elbo_pixelated_psf.jl"))
+DeterministicVIImagePSF.elbo_likelihood_with_fft!(ea_fft, 2, fsm_vec);
 
-benchmark_data = benchmark_comparison_data(single_infer_variational_parameters,
-                                           joint_infer_variational_parameters,
-                                           header)
+ea_fft.elbo_vars.elbo
 
-target_sources = collect(1:length(catalog_entries));
+elbo_fft = elbo_fft_opt(ea_fft);
 
-# Create the neighbor map (everyone is a neighbor of each other)
-neighbor_map = Infer.find_neighbors(target_sources, catalog_entries, band_images)
+ea_fft = ElboArgs(images, deepcopy(vp), patches, [1], psf_K=1);
+Celeste.Infer.load_active_pixels!(images, patches; exclude_nan=false);
+f_evals_fft, max_f_fft, max_x_fft, nm_result_fft =
+    DeterministicVI.maximize_f(elbo_fft_opt, ea_fft,
+                               verbose=true, max_iters=200);
+vp_opt_fft = deepcopy(ea_fft.vp[1])
 
-cat_local = vcat(entry, neighbors)
-vp = Vector{Float64}[init_source(ce) for ce in cat_local]
-patches = Infer.get_sky_patches(images, cat_local)
-Infer.load_active_pixels!(images, patches)
+#############
+# Print
+print_params(vp_opt_fft)
 
-ea = ElboArgs(images, vp, patches, [1])
-f_evals, max_f, max_x, nm_result = maximize_f(elbo, ea)
-vp[1]
+#############
+# Check that each is finding its respective optimum
+
+ea_fft_check = deepcopy(ea_fft);
+ea_fft_check.vp[1] = deepcopy(vp_opt);
+ea_fft.vp[1] = vp_opt_fft;
+println("FFT improvement:")
+elbo_fft_opt(ea_fft).v[] - elbo_fft_opt(ea_fft_check).v[]
+
+ea_check = deepcopy(ea);
+ea_check.vp[1] = deepcopy(vp_opt_fft);
+ea.vp[1] = deepcopy(vp_opt);
+println("Ordinary improvement:")
+DeterministicVI.elbo(ea).v[] - DeterministicVI.elbo(ea_check).v[]
+
