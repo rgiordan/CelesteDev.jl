1a80a13de8a365c776c1c718a86ad3b43432eb80
Ryan Giordano <rgiordan@gmail.com>
2016-12-05 14:20:11 +0100

    Off by a constant factor

--- rasterized_psf/fft_elbo_test.jl
+++ rasterized_psf/fft_elbo_test.jl
@@ -23,15 +23,16 @@ using Distributions
 using PyPlot
 
 
-images, ea, bodies = gen_two_body_dataset();
-s = 1
+function test_two_body()
+    images, ea, bodies = gen_two_body_dataset();
+    s = 1
 
-ea_fft, fsm_vec = DeterministicVIImagePSF.initialize_fft_elbo_parameters(
-    images, deepcopy(ea.vp), ea.patches, [s], use_raw_psf=false);
-elbo_fft_opt = DeterministicVIImagePSF.get_fft_elbo_function(ea_fft, fsm_vec, 2);
-
-elbo_fft_opt(ea_fft);
-f_evals, max_f, max_x, nm_result = DeterministicVI.maximize_f(elbo_fft_opt, ea_fft);
+    ea_fft, fsm_vec = DeterministicVIImagePSF.initialize_fft_elbo_parameters(
+        images, deepcopy(ea.vp), ea.patches, [s], use_raw_psf=false);
+    elbo_fft_opt = DeterministicVIImagePSF.get_fft_elbo_function(ea_fft, fsm_vec, 2);
+    elbo_fft_opt(ea_fft);
+    f_evals, max_f, max_x, nm_result = DeterministicVI.maximize_f(elbo_fft_opt, ea_fft);
+end
 
 
 function verify_sample_star(vs, pos)
@@ -74,29 +75,38 @@ function verify_sample_galaxy(vs, pos)
 end
 
 
-images, ea, body = gen_sample_star_dataset();
-ea.vp[1][ids.a[:, 1]] = [0.8, 0.2]
-ea_fft, fsm_vec = DeterministicVIImagePSF.initialize_fft_elbo_parameters(
-    images, deepcopy(ea.vp), ea.patches, [s], use_raw_psf=false);
-elbo_fft_opt = DeterministicVIImagePSF.get_fft_elbo_function(ea_fft, fsm_vec, 1);
-DeterministicVI.maximize_f(elbo_fft_opt, ea_fft; loc_width=1.0);
-verify_sample_star(ea_fft.vp[1], [10.1, 12.2]);
+# images, ea, body = gen_sample_star_dataset();
+# ea.vp[1][ids.a[:, 1]] = [0.8, 0.2]
+# ea_fft, fsm_vec = DeterministicVIImagePSF.initialize_fft_elbo_parameters(
+#     images, deepcopy(ea.vp), ea.patches, [s], use_raw_psf=false);
+# elbo_fft_opt = DeterministicVIImagePSF.get_fft_elbo_function(ea_fft, fsm_vec, 1);
+# DeterministicVI.maximize_f(elbo_fft_opt, ea_fft; loc_width=1.0);
+# verify_sample_star(ea_fft.vp[1], [10.1, 12.2]);
+# 
+# 
+# images, ea, body = gen_sample_galaxy_dataset();
+# ea_fft, fsm_vec = DeterministicVIImagePSF.initialize_fft_elbo_parameters(
+#     images, deepcopy(ea.vp), ea.patches, [s], use_raw_psf=false);
+# elbo_fft_opt = DeterministicVIImagePSF.get_fft_elbo_function(ea_fft, fsm_vec, 2);
+# DeterministicVI.maximize_f(elbo_fft_opt, ea_fft; loc_width=1.0);
+# verify_sample_galaxy(ea_fft.vp[1], [8.5, 9.6]);
 
+
+s = 1
+b = 3
 
 images, ea, body = gen_sample_galaxy_dataset();
 ea_fft, fsm_vec = DeterministicVIImagePSF.initialize_fft_elbo_parameters(
     images, deepcopy(ea.vp), ea.patches, [s], use_raw_psf=false);
-elbo_fft_opt = DeterministicVIImagePSF.get_fft_elbo_function(ea_fft, fsm_vec, 2);
-DeterministicVI.maximize_f(elbo_fft_opt, ea_fft; loc_width=1.0);
-verify_sample_galaxy(ea_fft.vp[1], [8.5, 9.6]);
+# DeterministicVIImagePSF.debug_populate_fsm_vec!(ea_fft, fsm_vec, 1);
 
+image_fft = CelesteEDA.render_source_fft(ea_fft, fsm_vec, s, b, include_epsilon=false);
+image_orig = CelesteEDA.render_source(ea, s, b, include_epsilon=false);
 
-
-
-
-
-
-
+# They are off by a constant factor.
+matshow(image_fft - image_orig);
+matshow(image_fft ./ image_orig);
+plot(image_fft[:], image_orig[:], "k.")
 
 
 
